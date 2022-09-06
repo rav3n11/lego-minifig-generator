d58dbad6b50973b6cae1a13ec1344f0049650cd5
Bereket Terefe <berelliant11@gmail.com>
2022-09-06 22:26:19 +0300

    [ feat ] - more fresh faces - randomization added

--- generate.js
+++ generate.js
@@ -3,15 +3,99 @@ const template = require("./templates")
 const styles = require("./styles")
 
 
-let head_color = `#1d3f3a`;
-let torso_color = `#522522`;
-let hand_color = `#1d3f3a`;
-let jeans_back_color = `#444444`;
-let jeans_front_color = `#444444`;
-let jeans_dark_color = `#000000`;
+
 
 const skin_colors = ['#F6D7B3', '#D09168', '#AA7D55', '#582A12']
+const lego_colors = [
+  '#05131D',
+  '#0055BF',
+  '#237841',
+  '#008F9B',
+  '#C91A09',
+  '#C870A0',
+  '#583927',
+  '#9BA19D',
+  '#6D6E5C',
+  '#B4D2E3',
+  '#4B9F4A',
+  '#55A5AF',
+  '#F2705E',
+  '#FC97AC',
+  '#F2CD37',
+  '#FFFFFF',
+  '#C2DAB8',
+  '#FBE696',
+  '#E4CD9E',
+  '#C9CAE2',
+  '#D4D5C9',
+  '#81007B',
+  '#2032B0',
+  '#FE8A18',
+  '#923978',
+  '#BBE90B',
+  '#958A73',
+  '#E4ADC8',
+  '#AC78BA',
+  '#E1D5ED',
+  '#635F52',
+  '#0020A0',
+  '#84B68D',
+  '#D9E4A7',
+  '#AEEFEC',
+  '#F8F184',
+  '#C1DFF0',
+  '#DF6695'
+]
 
+const faces = [
+  template.angry,
+  template.aviator,
+  template.beard,
+  template.chad,
+  template.death,
+  template.freddie,
+  template.happy,
+  template.lady,
+  template.love_eyes,
+  template.mexican,
+  template.mustache,
+  template.pain,
+  template.sad,
+  template.sad_lady,
+  template.smug,
+  template.snowman,
+  template.tired,
+  template.tongue,
+  template.wink
+]
+const jeans_color = [
+ '#3a80e2', '#104fc8', '#042ba7'
+]
+const black_pants_color = [
+  '#444444', '#444444', '#000000'
+]
+
+const chooseFace = () => {
+  let num = Math.floor(Math.random() * faces.length)
+  console.log(num)
+  return faces[num]
+}
+
+const chooseSkinColor = () => {
+  return skin_colors[Math.floor(Math.random() * skin_colors.length)]
+}
+
+const chooseLegoColor = () => {
+  return lego_colors[Math.floor(Math.random() * lego_colors.length)]
+}
+const choosePants = () => {
+  const num = Math.floor(Math.random() * lego_colors.length)
+  return num % 2 === 0 ? jeans_color : black_pants_color
+}
+
+let skin_color = chooseSkinColor();
+let torso_color = chooseLegoColor();
+let pants_color = choosePants();
 
 const result = `
   <svg 
@@ -21,12 +105,12 @@ const result = `
   viewBox="0 0 240 400">
       <defs>
           <style>
-              .head{fill:${head_color};}
+              .head{fill:${skin_color};}
               .torso{fill:${torso_color};}
-              .hand{fill:${hand_color};}
-              .jeans-back{fill: ${jeans_back_color};}
-              .jeans-front{fill: ${jeans_front_color};}
-              .jeans-dark{fill: ${jeans_dark_color};}
+              .hand{fill:${skin_color};}
+              .jeans-back{fill: ${pants_color[0]};}
+              .jeans-front{fill: ${pants_color[1]};}
+              .jeans-dark{fill: ${pants_color[2]};}
               ${styles.sad_l_style}
               ${styles.love_e_style}
               ${styles.freddie_style}
@@ -37,12 +121,20 @@ const result = `
               ${styles.tired_style}
               ${styles.dead_style}
               ${styles.snow_style}
+              ${styles.lady_style}
+              ${styles.mx_style}              
+              ${styles.happy_style}              
+              ${styles.sad_style}              
+              ${styles.angry_Style}              
+              ${styles.smug_style}              
+              ${styles.wink_style}              
+              ${styles.chad_style}              
           </style>
       </defs>
       ${template.legs}
       ${template.torso}
       ${template.head}
-      ${template.snowman}    
+      ${chooseFace()}    
   </svg>
   `;
 
--- styles.js
+++ styles.js
@@ -44,562 +44,546 @@ exports.mx_style = `
 }
 
 .mx-6 {
-    fill: #5a8096;
-}
-
-.mx-7 {
     fill: #503d4e;
 }
 
-.mx-8 {
+.mx-7 {
     fill: #111f28;
 }
 
-.mx-9 {
+.mx-8 {
     fill: #443b4b;
 }
 
-.mx-10 {
+.mx-9 {
     fill: #6796b3;
 }
 
-.mx-11 {
+.mx-10 {
     fill: #699bb4;
 }
 
-.mx-12 {
+.mx-11 {
     fill: #506b77;
 }
 
-.mx-13 {
+.mx-12 {
     fill: #4b94bf;
 }
 
-.mx-14 {
+.mx-13 {
     fill: #4a7a8c;
 }
 
-.mx-15 {
+.mx-14 {
     fill: #4994c1;
 }
 
-.mx-16 {
+.mx-15 {
     fill: #66acd6;
 }
 
-.mx-17 {
+.mx-16 {
     fill: #4b94c0;
 }
 
-.mx-18 {
+.mx-17 {
     fill: #478fb3;
 }
 
-.mx-19 {
+.mx-18 {
     fill: #4c91b4;
 }
 
-.mx-20 {
+.mx-19 {
     fill: #2f1626;
 }
 
-.mx-21 {
+.mx-20 {
     fill: #271b2b;
 }
 
-.mx-22 {
+.mx-21 {
     fill: #4c91b8;
 }
 
-.mx-23 {
+.mx-22 {
     fill: #40182c;
 }
 
-.mx-24 {
+.mx-23 {
     fill: #db494f;
 }
 
-.mx-25 {
+.mx-24 {
     fill: #ce2026;
 }
 
-.mx-26 {
+.mx-25 {
     fill: #f6908e;
 }
 
-.mx-27 {
+.mx-26 {
     fill: #df95a0;
 }
 
-.mx-28 {
+.mx-27 {
     fill: #fa8b9b;
 }
 
-.mx-29 {
+.mx-28 {
     fill: #49485c;
 }
 
-.mx-30 {
+.mx-29 {
     fill: #cb3a42;
 }
 
-.mx-31 {
+.mx-30 {
     fill: #82505e;
 }
 
-.mx-32 {
+.mx-31 {
     fill: #5796bc;
 }
 
-.mx-33 {
+.mx-32 {
     fill: #2f1e2a;
 }
 
-.mx-34 {
+.mx-33 {
     fill: #091629;
 }
 
-.mx-35 {
+.mx-34 {
     fill: #5696ca;
 }
 
-.mx-36 {
+.mx-35 {
     fill: #34283d;
 }
 
-.mx-37 {
+.mx-36 {
     fill: #283533;
 }
 
-.mx-38 {
+.mx-37 {
     fill: #4d92bd;
 }
 
-.mx-39 {
+.mx-38 {
     fill: #79b7da;
 }
 
-.mx-40 {
+.mx-39 {
     fill: #2e101f;
 }
 
-.mx-41 {
+.mx-40 {
     fill: #373a45;
 }
 
-.mx-42 {
+.mx-41 {
     fill: #6a9cc0;
 }
 
-.mx-43 {
+.mx-42 {
     fill: #55a7d7;
 }
 
-.mx-44 {
+.mx-43 {
     fill: #4d9dcf;
 }
 
-.mx-45 {
+.mx-44 {
     fill: #ad242b;
 }
 
-.mx-46 {
+.mx-45 {
     fill: #e77c87;
 }
 
-.mx-47 {
+.mx-46 {
     fill: #4d1b2c;
 }
 
-.mx-48 {
+.mx-47 {
     fill: #3d152a;
 }
 
-.mx-49 {
+.mx-48 {
     fill: #3f1929;
 }
 
-.mx-50 {
+.mx-49 {
     fill: #c30f0c;
 }
 
-.mx-51 {
+.mx-50 {
     fill: #060d21;
 }
 
-.mx-52 {
+.mx-51 {
     fill: #d73c40;
 }
 
-.mx-53 {
+.mx-52 {
     fill: #781e28;
 }
 
-.mx-54 {
+.mx-53 {
     fill: #d71118;
 }
 
-.mx-55 {
+.mx-54 {
     fill: #fe9c9e;
 }
 
-.mx-56 {
+.mx-55 {
     fill: #220f1d;
 }
 
-.mx-57 {
+.mx-56 {
     fill: #801720;
 }
 
-.mx-58 {
+.mx-57 {
     fill: #c55667;
 }
 
-.mx-59 {
+.mx-58 {
     fill: #e20915;
 }
 
-.mx-60 {
+.mx-59 {
     fill: #ef96a5;
 }
 
-.mx-61 {
+.mx-60 {
     fill: #f7a79d;
 }
 
-.mx-62 {
+.mx-61 {
     fill: #3d160c;
 }
 
-.mx-63 {
+.mx-62 {
     fill: #691d16;
 }
 
-.mx-64 {
+.mx-63 {
     fill: #d80c12;
 }
 
-.mx-65 {
+.mx-64 {
     fill: #d4313a;
 }
 
-.mx-66 {
+.mx-65 {
     fill: #4e1323;
 }
 
-.mx-67 {
+.mx-66 {
     fill: #360302;
 }
 
-.mx-68 {
+.mx-67 {
     fill: #840e0e;
 }
 
-.mx-69 {
+.mx-68 {
     fill: #bd1825;
 }
 
-.mx-70 {
+.mx-69 {
     fill: #a91226;
 }
 
-.mx-71 {
+.mx-70 {
     fill: #f6878a;
 }
 
-.mx-72 {
+.mx-71 {
     fill: #febabd;
 }
 
-.mx-73 {
+.mx-72 {
     fill: #feafa2;
 }
 
-.mx-74 {
+.mx-73 {
     fill: #f294a0;
 }
 
-.mx-75 {
+.mx-74 {
     fill: #f9aeab;
 }
 
-.mx-76 {
+.mx-75 {
     fill: #fea8ac;
 }
 
-.mx-77 {
+.mx-76 {
     fill: #de4254;
 }
 
-.mx-78 {
+.mx-77 {
     fill: #dd3c47;
 }
 
-.mx-79 {
+.mx-78 {
     fill: #f38b91;
 }
 
-.mx-80 {
+.mx-79 {
     fill: #f79a97;
 }
 
-.mx-81 {
+.mx-80 {
     fill: #febcc0;
 }
 
-.mx-82 {
+.mx-81 {
     fill: #fca3a4;
 }
 
-.mx-83 {
+.mx-82 {
     fill: #d9515e;
 }
 
-.mx-84 {
+.mx-83 {
     fill: #fac3b1;
 }
 
-.mx-85 {
+.mx-84 {
     fill: #f78d8d;
 }
 
-.mx-86 {
+.mx-85 {
     fill: #df0814;
 }
 
-.mx-87 {
+.mx-86 {
     fill: #fe8a96;
 }
 
-.mx-88 {
+.mx-87 {
     fill: #c53e41;
 }
 
-.mx-89 {
+.mx-88 {
     fill: #e34d53;
 }
 
-.mx-90 {
+.mx-89 {
     fill: #e84a55;
 }
 
-.mx-91 {
+.mx-90 {
     fill: #df5454;
 }
 
-.mx-92 {
+.mx-91 {
     fill: #fa7671;
 }
 
-.mx-93 {
+.mx-92 {
     fill: #fb7a80;
 }
 
-.mx-94 {
-    fill: #413402;
-}
-
-.mx-95 {
-    fill: #4e3c03;
-}
-
-.mx-96 {
+.mx-93 {
     fill: #420e1b;
 }
 
-.mx-97 {
+.mx-94 {
     fill: #fe8b97;
 }
 
-.mx-98 {
+.mx-95 {
     fill: #fea0a5;
 }
 
-.mx-99 {
+.mx-96 {
     fill: #ed9892;
 }
 
-.mx-100 {
+.mx-97 {
     fill: #531e14;
 }
 
-.mx-101 {
+.mx-98 {
     fill: #f0545f;
 }
 
-.mx-102 {
+.mx-99 {
     fill: #501e08;
 }
 
-.mx-103 {
+.mx-100 {
     fill: #ae292e;
 }
 
-.mx-104 {
+.mx-101 {
     fill: #f09ba0;
 }
 
-.mx-105 {
+.mx-102 {
     fill: #dc050d;
 }
 
-.mx-106 {
+.mx-103 {
     fill: #451202;
 }
 
-.mx-107 {
+.mx-104 {
     fill: #a42129;
 }
 
-.mx-108 {
+.mx-105 {
     fill: #590400;
 }
 
-.mx-109 {
+.mx-106 {
     fill: #f8747d;
 }
 
-.mx-110 {
+.mx-107 {
     fill: #fec3c3;
 }
 
-.mx-111 {
+.mx-108 {
     fill: #febbb6;
 }
 
-.mx-112 {
+.mx-109 {
     fill: #feb4bc;
 }
 
-.mx-113 {
+.mx-110 {
     fill: #f69593;
 }
 
-.mx-114 {
+.mx-111 {
     fill: #db161f;
 }
 
-.mx-115 {
+.mx-112 {
     fill: #feb3c5;
 }
 
-.mx-116 {
+.mx-113 {
     fill: #febbb9;
 }
 
-.mx-117 {
+.mx-114 {
     fill: #521104;
 }
 
-.mx-118 {
+.mx-115 {
     fill: #c61115;
 }
 
-.mx-119 {
+.mx-116 {
     fill: #e13744;
 }
 
-.mx-120 {
+.mx-117 {
     fill: #fea6a5;
 }
 
-.mx-121 {
+.mx-118 {
     fill: #fe96a2;
 }
 
-.mx-122 {
+.mx-119 {
     fill: #de8987;
 }
 
-.mx-123 {
+.mx-120 {
     fill: #f98b8e;
 }
 
-.mx-124 {
+.mx-121 {
     fill: #fe9ea4;
 }
 
-.mx-125 {
+.mx-122 {
     fill: #fe8a8f;
 }
 
-.mx-126 {
+.mx-123 {
     fill: #feafa0;
 }
 
-.mx-127 {
+.mx-124 {
     fill: #9a494f;
 }
 
-.mx-128 {
+.mx-125 {
     fill: #fba0a2;
 }
 
-.mx-129 {
+.mx-126 {
     fill: #fec3c7;
 }
 
-.mx-130 {
+.mx-127 {
     fill: #e28b8f;
 }
 
-.mx-131 {
+.mx-128 {
     fill: #bb5c65;
 }
 
-.mx-132 {
+.mx-129 {
     fill: #fa869a;
 }
 
-.mx-133 {
+.mx-130 {
     fill: #fea9b9;
 }
 
-.mx-134 {
+.mx-131 {
     fill: #501608;
 }
 
-.mx-135 {
+.mx-132 {
     fill: #3d0d04;
 }
 
-.mx-136 {
+.mx-133 {
     fill: #4c1103;
 }
 
-.mx-137 {
+.mx-134 {
     fill: #dc8085;
 }
 
-.mx-138 {
+.mx-135 {
     fill: #161315;
 }
 
-.mx-139 {
+.mx-136 {
     fill: #1c1816;
 }
 
-.mx-140 {
+.mx-137 {
     fill: #311a15;
 }
 
-.mx-141 {
+.mx-138 {
     fill: #3b220a;
 }
 
-.mx-142 {
+.mx-139 {
     fill: #351717;
 }
 
-.mx-143 {
-    fill: #3e3d27;
-}
-
-.mx-144 {
+.mx-140 {
     fill: #bfc1c6;
 }
 
-.mx-145 {
+.mx-141 {
     fill: #b7c1c4;
 }
 `
@@ -1045,5 +1029,276 @@ exports.snow_style = `
 
 .snow-19 {
     fill: #f2222d;
+}
+`
+exports.lady_style = `
+.lady-1 {
+    fill: #080505;
+}
+
+.lady-2 {
+    fill: #764029;
+}
+
+.lady-3 {
+    fill: #0b0806;
+}
+
+.lady-4 {
+    fill: #6e3d2a;
+}
+
+.lady-5 {
+    fill: #221716;
+}
+
+.lady-6 {
+    fill: #ad5a33;
+}
+
+.lady-7 {
+    fill: #ae5834;
+}
+
+.lady-8 {
+    fill: #ae5a35;
+}
+
+.lady-9 {
+    fill: #a85c33;
+}
+
+.lady-10 {
+    fill: #7c3e1f;
+}
+
+.lady-11 {
+    fill: #923f1f;
+}
+
+.lady-12 {
+    fill: #954c2a;
+}
+
+.lady-13 {
+    fill: #b66235;
+}
+
+.lady-14 {
+    fill: #82401e;
+}
+
+.lady-15 {
+    fill: #854412;
+}
+
+.lady-16 {
+    fill: #86411c;
+}
+
+.lady-17 {
+    fill: #994220;
+}
+
+.lady-18 {
+    fill: #8d4a16;
+}
+
+.lady-19 {
+    fill: #c57145;
+}
+
+.lady-20 {
+    fill: #8f4b1b;
+}
+
+.lady-21 {
+    fill: #924621;
+}
+
+.lady-22 {
+    fill: #ae5732;
+}
+
+.lady-23 {
+    fill: #9a461f;
+}
+
+.lady-24 {
+    fill: #833e18;
+}
+
+.lady-25 {
+    fill: #cf2a04;
+}
+
+.lady-26 {
+    fill: #6e0000;
+}
+
+.lady-27 {
+    fill: #a32508;
+}
+
+.lady-28 {
+    fill: #e5ddd3;
+}
+
+.lady-29 {
+    fill: #e6e8ea;
+}
+`
+exports.happy_style = `
+.happy-1 {
+    fill: #babab6;
+}
+
+.happy-2 {
+    fill: #edecea;
+}
+
+.happy-3 {
+    fill: #ac2b29;
+}
+
+.happy-4 {
+    fill: #522808;
+}
+
+.happy-5 {
+    fill: #cbc7c2;
+}
+`
+exports.sad_style = `
+.sad-1 {
+    opacity: 0.4;
+}
+
+.sad-2 {
+    fill: #7994b3;
+}
+
+.sad-3 {
+    fill: #c6c6c0;
+}
+
+.sad-4 {
+    fill: #c0bec0;
+}
+`
+exports.angry_Style = `
+.angry-1 {
+    fill: #e7e8e5;
+}
+
+.angry-2 {
+    fill: #e4e7e6;
+}
+
+.angry-3 {
+    fill: #bfc4bf;
+}
+
+.angry-4 {
+    fill: #e1ece3;
+}
+`
+exports.smug_style = `
+.smug-1 {
+    fill: #c6cec7;
+}
+
+.smug-2 {
+    fill: #ccd0cc;
+}
+
+.smug-3 {
+    fill: #100b07;
+}
+
+.smug-4 {
+    fill: #e5e3e3;
+}
+`
+exports.wink_style = `
+.wink-1 {
+    fill: #d5d7d8;
+}
+`
+exports.chad_style = `
+.chad-1 {
+    fill: #77442c;
+}
+
+.chad-2 {
+    fill: #7a422b;
+}
+
+.chad-3 {
+    fill: #7c4824;
+}
+
+.chad-4 {
+    fill: #88491f;
+}
+
+.chad-5 {
+    fill: #b05a33;
+}
+
+.chad-6 {
+    fill: #824225;
+}
+
+.chad-7 {
+    fill: #804629;
+}
+
+.chad-8 {
+    fill: #c57249;
+}
+
+.chad-9 {
+    fill: #8f4a29;
+}
+
+.chad-10 {
+    fill: #8e4f18;
+}
+
+.chad-11 {
+    fill: #aa5933;
+}
+
+.chad-12 {
+    fill: #954c2f;
+}
+
+.chad-13 {
+    fill: #884224;
+}
+
+.chad-14 {
+    fill: #aa5938;
+}
+
+.chad-15 {
+    fill: #c37147;
+}
+
+.chad-16 {
+    fill: #994a24;
+}
+
+.chad-17 {
+    fill: #cfcfc4;
+}
+
+.chad-18 {
+    fill: #a3998d;
+}
+
+.chad-19 {
+    fill: #c5bcb7;
 }
 `
